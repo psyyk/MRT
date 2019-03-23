1878d1f3f27dfd046345bf569369ad69d6b74172
Pi <psyyk@nottingham.ac.uk>
2019-03-23 15:32:58 +0000

    add data

--- test.py
+++ test.py
@@ -26,7 +26,7 @@ while True:
     time.sleep(0.01)
     #print(median)
     
-    if (highPassed-median) > 20:
+    if (highPassed-median) > 10:
         outputBuffer.append(1)
         if sum(outputBuffer) >= 3:
             #print("warning! " + str(highPassed-median)+" PIR: "+str(pir)+" Ultra: "+str(ultra))
@@ -34,6 +34,7 @@ while True:
 	    warnBuffer.append(1)
             count+=1
 	if sum(warnBuffer) >=2:
+
 	    print("warning! " + str(highPassed-median)+" PIR: "+str(pir)+" Ultra: "+str(ultra))
     else:
         outputBuffer.append(0)
